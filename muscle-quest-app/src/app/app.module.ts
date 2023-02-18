bf72718540e0be8df445aad47bd4531a6de2460c
SloaneTribble <sloanetribble@gmail.com>
2023-02-18 14:25:24 -0800

    Remove boilerplate HTML, add basic Sloane level component

--- muscle-quest-app/src/app/app.module.ts
+++ muscle-quest-app/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SloaneLvlOneComponent } from './sloane-lvl-one/sloane-lvl-one.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SloaneLvlOneComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
