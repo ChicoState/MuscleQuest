d8041215934434c9a97860d28208a0bf4dbe838e
SloaneTribble <100537368+SloaneTribble@users.noreply.github.com>
2023-02-18 19:04:16 -0800

    Basic routing (#16)
    * Begin implementing basic routing
    
    * Set default URL to start screen, add link to and from sloane level

--- muscle-quest-app/src/app/app-routing.module.ts
+++ muscle-quest-app/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 import { SloaneLvlOneComponent } from './sloane-lvl-one/sloane-lvl-one.component';
 
 const routes: Routes = [
-  {
-    path: 'sloane-lvl-one',
-    component: SloaneLvlOneComponent,
-  },
+  { path: '', redirectTo: '/start', pathMatch: 'full' },
+  { path: 'start', component: StartScreenComponent },
+  { path: 'sloane-lvl-one', component: SloaneLvlOneComponent },
 ];
 
 @NgModule({
--- muscle-quest-app/src/app/sloane-lvl-one/sloane-lvl-one.component.ts
+++ muscle-quest-app/src/app/sloane-lvl-one/sloane-lvl-one.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-sloane-lvl-one',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./sloane-lvl-one.component.css'],
 })
 export class SloaneLvlOneComponent {
+  constructor(private location: Location) {}
+
   title = 'The Revenge of Time';
+
+  goBack(): void {
+    this.location.back();
+  }
 }
