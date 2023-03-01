9372fe4ed7706bd8b184d5bad05e8274fe2f4aa4
isaelpuentes <98490391+isaelpuentes@users.noreply.github.com>
2023-03-01 14:06:01 -0800

    Add files via upload

---
+++ muscle-quest-app/src/app/back-day/back-day.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back-day',
+  templateUrl: './back-day.component.html',
+  styleUrls: ['./back-day.component.css']
+})
+export class BackDayComponent {
+
+}
---
+++ muscle-quest-app/src/app/chest-day/chest-day.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chest-day',
+  templateUrl: './chest-day.component.html',
+  styleUrls: ['./chest-day.component.css']
+})
+export class ChestDayComponent {
+
+}
