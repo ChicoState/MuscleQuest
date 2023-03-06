3e6cd2eb450c15f9be6f2c1c687fdb7b0c39b848
Caleb Besser <cebesser@csuchico.edu>
2023-03-06 10:30:25 -0800

    Havent fixed back button yet

--- muscle-quest-app/src/app/start-screen/start-screen.component.ts
+++ muscle-quest-app/src/app/start-screen/start-screen.component.ts
@@ -13,73 +13,6 @@ export class StartScreenComponent {
   
 }
 
-function switchToLevels(elementList: HTMLElement[], state: number){
-  elementList[0].classList.toggle('slideFromLeft');
-  elementList[1].classList.toggle('slideFromLeft-2');
-  elementList[2].classList.toggle('slideFromLeft-3');
-  elementList[3].classList.toggle('slideFromLeft-4');
-  elementList[0].classList.toggle('slideToRight');
-  elementList[1].classList.toggle('slideToRight-2');
-  elementList[2].classList.toggle('slideToRight-3');
-  elementList[3].classList.toggle('slideToRight-4');
-  if(state == 1) {
-    elementList[3].addEventListener("animationend", () => {
-      for(let i = 0; i < 4; i++){
-        console.log(state);
-        elementList[i].style.display = "none";
-        elementList[i].style.opacity = "0";
-      }
-    })
-  }
-  
-  elementList[8].style.display = "block";
-  elementList[4].classList.toggle('slideFromLeft');
-  elementList[5].classList.toggle('slideFromLeft-2');
-  elementList[6].classList.toggle('slideFromLeft-3');
-  elementList[7].classList.toggle('slideFromLeft-4');
-  if(state == 1){
-    for(let i = 4; i < 8; i++){
-      elementList[i].style.opacity = "1";
-    }
-  }
-  state = 10;
-}
-function switchToOptions(elementList: HTMLElement[], state: number){
-  elementList[4].classList.toggle('slideFromLeft');
-  elementList[5].classList.toggle('slideFromLeft-2');
-  elementList[6].classList.toggle('slideFromLeft-3');
-  elementList[7].classList.toggle('slideFromLeft-4');
-  elementList[4].classList.toggle('slideToRight');
-  elementList[5].classList.toggle('slideToRight-2');
-  elementList[6].classList.toggle('slideToRight-3');
-  elementList[7].classList.toggle('slideToRight-4');
-  if(state == 0){
-    elementList[7].addEventListener("animationend", () => {
-      for(let i = 4; i < 8; i++){
-        elementList[i].style.display = "none";
-        elementList[i].style.opacity = "0";
-      }
-      elementList[8].style.display = "none";
-    })
-  }
-  
-  elementList[0].classList.toggle('slideToRight');
-  elementList[1].classList.toggle('slideToRight-2');
-  elementList[2].classList.toggle('slideToRight-3');
-  elementList[3].classList.toggle('slideToRight-4');
-  elementList[0].classList.toggle('slideFromLeft');
-  elementList[1].classList.toggle('slideFromLeft-2');
-  elementList[2].classList.toggle('slideFromLeft-3');
-  elementList[3].classList.toggle('slideFromLeft-4');
-  if(state == 0) {
-    for(let i = 0; i < 4; i++){
-      elementList[i].style.display = "block";
-      elementList[i].style.opacity = "1";
-    }
-  }
-  
-}
-
 window.onload = function() {
   let levelsOption = document.getElementById("levels")!; //0
   let characterOption = document.getElementById("character")!;//1
@@ -88,31 +21,58 @@ window.onload = function() {
   let sloaneLevel = document.getElementById('revenge')!;//4
   let peterLevel = document.getElementById('orc')!;//5
   let isaLevel = document.getElementById('isa')!;//6
-  let backButton = document.getElementById('back')!;//7
+  let backButton = document.getElementById('levelBack')!;//7
   let levelContainer = document.getElementById('levelContainer')!;//8
   let logoDiv = document.getElementById('logoContainer')!;//9
-  let state = 0;
 
 
   let elementList = [levelsOption, characterOption, shopOption, settingsOption, sloaneLevel, peterLevel, isaLevel, backButton, levelContainer, logoDiv];
 
 
-  levelsOption.classList.toggle('slideFromLeft');
-  characterOption.classList.toggle('slideFromLeft-2');
-  shopOption.classList.toggle('slideFromLeft-3');
-  settingsOption.classList.toggle('slideFromLeft-4');
-  logoDiv.classList.toggle('fadeIn');
-  // for(let i = 0; i < 4; i++){
-  //   elementList[i].style.opacity = "1";
-  // }
-
-  levelsOption.addEventListener("click", () => {
-    state = 1;
-    switchToLevels(elementList, state);
+  for(let i = 0; i < 4; i++){
+    elementList[i].classList.toggle("slideFromLeft");
+    elementList[i].addEventListener("animationend", () => {
+      elementList[i].style.opacity = "1";
+      elementList[i].style.display = "block";
+    }, {once: true})
+  }
+  elementList[9].classList.toggle("fadeIn");
+  elementList[0].addEventListener("click", () =>{
+    elementList[8].style.display = "block";
+    for(let i = 0; i < 4; i++){
+      elementList[i].classList.replace("slideFromLeft", "slideToRight");
+      elementList[i].addEventListener("animationend", () => {
+        elementList[i].style.opacity = "0";
+        elementList[i].style.display = "none";
+      }, {once: true})
+    }
+    for(let i = 4; i < 8; i++){
+      elementList[i].classList.toggle("slideFromLeft");
+      elementList[i].addEventListener("animationend", () => {
+        elementList[i].style.opacity = "1";
+        elementList[i].style.display = "block";
+      }, {once: true})
+    }
   })
-  backButton.addEventListener("click", () => {
-    state = 0;
-    switchToOptions(elementList, state);
+  elementList[7].addEventListener("click", () => {
+    for(let i = 4; i < 8; i++){
+      elementList[i].classList.replace("slideFromLeft", "slideToRight");
+      elementList[i].addEventListener("animationend", () => {
+        elementList[i].style.opacity = "0";
+        elementList[i].style.display = "none";
+      }, {once: true})
+    }
+    for(let i = 0; i < 4; i++){
+      elementList[i].classList.replace("slideToRight", "slideFromLeft");
+      elementList[i].addEventListener("animationend", () => {
+        elementList[i].style.opacity = "1";
+        elementList[i].style.display = "block";
+      }, {once: true})
+    }
+    elementList[8].classList.toggle('wait1s');
+    elementList[8].addEventListener("animationend", () => {
+      elementList[8].style.display = "none";
+    })
   })
 }
 
