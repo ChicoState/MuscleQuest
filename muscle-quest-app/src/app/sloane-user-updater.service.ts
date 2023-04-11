e85781268a980a3bca1e5f5d325ea7ab3db2c470
SloaneTribble <sloanetribble@gmail.com>
2023-04-11 14:10:40 -0700

    Continue implementing CRUD service

--- muscle-quest-app/src/app/sloane-user-updater.service.ts
+++ muscle-quest-app/src/app/sloane-user-updater.service.ts
@@ -2,22 +2,27 @@ import { Injectable } from '@angular/core';
 import {
   AngularFirestore,
   AngularFirestoreCollection,
+  AngularFirestoreDocument,
 } from '@angular/fire/compat/firestore';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { onAuthStateChanged, getAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { DataObject, Material, Element } from 'src/lib/user';
 import { DEFAULT_USER_DATA } from 'src/lib/user';
+import { ItemState } from 'src/lib/user';
 
 @Injectable({
   providedIn: 'root',
 })
-export class FirebaseDataService {
+export class SloaneUserUpdateService {
   userCollection: AngularFirestoreCollection<DataObject>;
   authState = getAuth();
+  userDoc!: AngularFirestoreDocument<DataObject>; // add ! to indicate that it will be initialized in ngOnInit
+  uid = '';
+  userData: DataObject = DEFAULT_USER_DATA;
 
-  constructor(private afs: AngularFirestore) {
+  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) {
     this.userCollection = afs.collection<DataObject>('users');
   }
 
@@ -25,6 +30,8 @@ export class FirebaseDataService {
     onAuthStateChanged(this.authState, (user) => {
       if (user) {
         console.log(user.uid);
+        this.uid = user.uid;
+        this.userDoc = this.afs.doc<DataObject>(`users/${user.uid}`);
 
         this.checkIfUserExists(user.uid).subscribe((exists) => {
           if (exists) {
@@ -34,6 +41,7 @@ export class FirebaseDataService {
               'This is a new user. Generating a new object for this user.'
             );
             let newUser: DataObject = DEFAULT_USER_DATA;
+            newUser.userId = user.uid; // set the userId of the new user object
             this.saveUser(newUser);
           }
         });
@@ -48,8 +56,33 @@ export class FirebaseDataService {
     return this.userCollection.doc(id).set(dataObject);
   }
 
+  getUserData(): Observable<DataObject> {
+    return this.userDoc.valueChanges().pipe(
+      map((data) => {
+        if (data === undefined) {
+          throw new Error('DataObject does not exist for this user');
+        } else {
+          return data;
+        }
+      })
+    );
+  }
+
   checkIfUserExists(userId: string): Observable<boolean> {
     const docRef = this.userCollection.doc(userId);
+    console.log(docRef);
+    console.log(
+      docRef.valueChanges().subscribe((data) => {
+        console.log(data);
+        data?.equipped;
+      })
+    );
     return docRef.get().pipe(map((docSnapshot) => docSnapshot.exists));
+  }
+
+  giveItem(item: ItemState) {
+    this.getUserData().pipe(tap((data) => (this.userData = data)));
+    this.userData.items.push(item);
+    this.saveUser(this.userData);
   }
 }
